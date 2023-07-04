d1e21dd5ef43b5e94c108879c95d2292f99ae34b
Burak Albayrak <burak_albayrak0@icloud.com>
2023-07-05 02:30:02 +0300

    Loops

---
+++ python_syntax/_5_Loops/main.py
@@ -0,0 +1,17 @@
+#while
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+
+#for
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
